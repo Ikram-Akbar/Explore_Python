98575e1d754ca3b775e7e6fd0d5847e98ad47795
Ikram Akbar <ikramakbar71.com>
2023-09-13 23:56:34 +0600

    default parameter

---
+++ Day__2/defaultParameter.py
@@ -0,0 +1,15 @@
+def sum(n1, n2):
+    result = n1 + n2
+    return result
+total = sum(33,12)
+print("total : ", total)
+
+#default Parameter: 
+def viewCount(option1,option2,option3=5):
+    return option1+option2+option3
+
+print(viewCount(12,14))
+
+        
+        
+        
