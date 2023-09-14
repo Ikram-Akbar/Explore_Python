4e21526a1f741bb322d2b36236e7fda56e716b63
Ikram Akbar <ikramakbar71.com>
2023-09-14 11:17:49 +0600

    args in python

--- Day__2/defaultParameter.py
+++ Day__2/defaultParameter.py
@@ -1,13 +1,19 @@
 def sum(n1, n2):
     result = n1 + n2
     return result
+
+
 total = sum(33, 12)
 print("total : ", total)
+
 
 # default Parameter:
 def viewCount(option1, option2, option3=5):
     return option1 + option2 + option3
+
+
 print(viewCount(12, 14))
+
 
 # Argument :
 def all_sum(*numbers):
@@ -16,7 +22,16 @@ def all_sum(*numbers):
     for num in numbers:
         print(num)
         sum += num
-        print("total sum of the tuple : ",sum)
-        
+        print("total sum of the tuple : ", sum)
+
+
 n = all_sum(5, 1, 4, 5)
 print(n)
+
+
+def new_func(*args):
+    print(args)
+
+
+my_Func = new_func(45, "string ", True)
+print("function invoke: ",my_Func)
